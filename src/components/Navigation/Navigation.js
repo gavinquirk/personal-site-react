378ac459480634c6d42e6df65d754a397a94a602
Gavin Quirk <gavinquirk@gmail.com>
2018-11-12 13:49:26 -0800

    navbar styling

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -11,6 +11,7 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem } from 'reactstrap';
+import './Navigation.css'
 
 export default class Navigation extends React.Component {
   constructor(props) {
@@ -29,22 +30,22 @@ export default class Navigation extends React.Component {
   render() {
     return (
       <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">GavinQuirk.com</NavbarBrand>
+        <Navbar className="navbar" light expand="md">
+          <NavbarBrand href="/"><span className="navbrand">GavinQuirk.com</span></NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/portfolio/">Portfolio</NavLink>
+                <NavLink href="/portfolio/"><span className="navlink">Portfolio</span></NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/contact">Contact</NavLink>
+                <NavLink href="/contact"><span className="navlink">Contact</span></NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                <NavLink href="https://github.com/reactstrap/reactstrap"><span className="navlink">GitHub</span></NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">Linkedin</NavLink>
+                <NavLink href="https://github.com/reactstrap/reactstrap"><span className="navlink">Linkedin</span></NavLink>
               </NavItem>
             </Nav>
           </Collapse>
