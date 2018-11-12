1146521bc6b40de69ee5e708917b60c75995e49e
Gavin Quirk <gavinquirk@gmail.com>
2018-11-12 14:53:00 -0800

    added skills section to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -17,15 +17,28 @@ class Home extends Component {
         <Navigation />
         <Banner />
         <div className="skills">
+          <hr />
+          <span className="skills-intro">
+            <h2 >I like to use...</h2>
+          </span>
+          <hr />
           <Row className="nomargin">
-            <Col lg={6} sm={12} className="nomargin">
+            <Col lg={4} sm={12} className="nomargin">
               <div className="skills-section nomargin">
-                <p>MERN</p>
+                <h4 className="skills-section-title">MERN Stack</h4>
+                <img src="/images/slackoverflow_640_320.png" />
               </div>
             </Col>
-            <Col lg={6} sm={12} className="nomargin">
+            <Col lg={4} sm={12} className="nomargin">
               <div className="skills-section nomargin">
-                <p>Wordpress</p>
+                <h4 className="skills-section-title">Wordpress</h4>
+                <img src="/images/fictionaluniversity_640_320.png" />
+              </div>
+            </Col>
+            <Col lg={4} sm={12} className="nomargin">
+              <div className="skills-section nomargin">
+                <h4 className="skills-section-title">Meteor</h4>
+                <img src="/images/fictionaluniversity_640_320.png" />
               </div>
             </Col>
           </Row>
