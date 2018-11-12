91f6ead7cfd774d24a6abddaa88667e9a05abb02
Gavin Quirk <gavinquirk@gmail.com>
2018-11-12 11:48:51 -0800

    switched from materialize to bootstrap, added navbar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,55 @@
-import React, { Component } from 'react';
-import { Navbar, NavItem, Icon } from 'react-materialize'
-import './Navigation.css'
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
-class Navigation extends Component {
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
-      <div className="Navbar">
-        <Navbar brand='logo' right className="navbar">
-          <NavItem href='/portfolio'><Icon>view_module</Icon></NavItem>
-          <NavItem href='/contact'><Icon>view_module</Icon></NavItem>
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">GavinQuirk.com</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/portfolio/">Portfolio</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/contact">Contact</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Linkedin</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
         </Navbar>
       </div>
     );
   }
 }
-
-export default Navigation;
--- src/components/PortfolioCard/PortfolioCard.js
+++ src/components/PortfolioCard/PortfolioCard.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
-import { Card, CardTitle } from 'react-materialize'
-import placeholder from './placeholder.jpg'
+// import React, { Component } from 'react';
+// import placeholder from './placeholder.jpg'
 
-class PortfolioCard extends Component {
-  render() {
-    return (
-      <div className="PortfolioCard">
-        <Card className="black" header={<CardTitle className="text-grey-lighten-5" reveal image={placeholder} waves='light' />}
-          title={this.props.project.title}
-          reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
-          <p><a href={this.props.project.github}>Github</a></p>
-        </Card>
-      </div>
-    );
-  }
-}
+// class PortfolioCard extends Component {
+//   render() {
+//     return (
+//       <div className="PortfolioCard">
+//         <Card className="black" header={<CardTitle className="text-grey-lighten-5" reveal image={placeholder} waves='light' />}
+//           title={this.props.project.title}
+//           reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
+//           <p><a href={this.props.project.github}>Github</a></p>
+//         </Card>
+//       </div>
+//     );
+//   }
+// }
 
-export default PortfolioCard;
+// export default PortfolioCard;
