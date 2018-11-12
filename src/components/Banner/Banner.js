5e7b577fcf9d7deba0c110e28ab23fabc007cc20
Gavin Quirk <gavinquirk@gmail.com>
2018-11-12 14:27:56 -0800

    added media queries to banner, css to home

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import './style.css'
+import './Banner.css'
 
 class Banner extends Component {
   render() {
     return (
       <div className="banner">
-        <span>Banner</span>
       </div>
     );
   }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import Navigation from '../../components/Navigation'
 import Banner from '../../components/Banner'
+import './Home.css';
+import { Row, Col, Container, Jumbotron } from 'reactstrap';
 
 class Home extends Component {
 
   componentDidMount() {
     console.log('HOME')
   }
-  
+
   render() {
 
     return (
-      <div className="Home">
+      <div className="home">
         <Navigation />
         <Banner />
-        <span>Home</span>
+        <div className="skills">
+          <Row className="nomargin">
+            <Col lg={6} sm={12} className="nomargin">
+              <div className="skills-section nomargin">
+                <p>MERN</p>
+              </div>
+            </Col>
+            <Col lg={6} sm={12} className="nomargin">
+              <div className="skills-section nomargin">
+                <p>Wordpress</p>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   }
