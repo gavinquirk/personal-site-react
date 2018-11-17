5e665e5c9b2328ca2507b69c1be867effcd89574
Gavin Quirk <gavinquirk@gmail.com>
2018-11-16 16:38:29 -0800

    added artallus page, contact page

---
+++ src/pages/Portfolio/projects/Artallus/Artallus.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Navigation from '../../components/Navigation'
+import Banner from '../../components/Banner'
+
+
+class Artallus extends Component {
+  render() {
+    return (
+      <div className="Contact">
+        <Navigation />
+        <span>Contact</span>
+      </div>
+    );
+  }
+}
+
+export default Artallus;
