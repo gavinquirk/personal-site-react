f98b7ebfb1aedc9f68f6b069cbf32b6af4219259
Gavin Quirk <gavinquirk@gmail.com>
2018-11-17 11:11:11 -0800

    webpack fix

--- src/components/Artallus/Artallus.js
+++ src/components/Artallus/Artallus.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
-import { Row, Col, Container, Jumbotron } from 'react-materialize';
-import './Artallus.css'
+// import React, { Component } from 'react';
+// import './Artallus.css'
 
-class Artallus extends Component {
+// class Artallus extends Component {
 
-  render() {
+//   render() {
 
-    return (
-      <Row className="nomargin UCI section">
-        <Col lg={6}>
-          <img className="section-image" src="images/Artallus-Logo.png"/>
-        </Col>
-        <Col lg={6}>
-          <div className="nar">
-            <h1 className="nar-p">I became an intern at Artallus in Aug 2018</h1>
-          </div>
-        </Col>
-      </Row>
-    );
-  }
-}
+//     return (
+//       <Row className="nomargin UCI section">
+//         <Col lg={6}>
+//           <img className="section-image" src="images/Artallus-Logo.png"/>
+//         </Col>
+//         <Col lg={6}>
+//           <div className="nar">
+//             <h1 className="nar-p">I became an intern at Artallus in Aug 2018</h1>
+//           </div>
+//         </Col>
+//       </Row>
+//     );
+//   }
+// }
 
-export default Artallus;
+// export default Artallus;
