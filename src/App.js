8e9d703c875208d334c7a2bccc8e9d3400abd089
Gavin Quirk <gavinquirk@gmail.com>
2018-11-16 19:44:00 -0800

    more material-ui changes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import Contact from './pages/Contact'
 import Portfolio from './pages/Portfolio'
 import './App.css'
 import 'typeface-roboto';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+
 
 
 import { BrowserRouter, Route } from 'react-router-dom'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#757ce8' }, // Purple and green play nicely together.
+    secondary: { main: '#757ce8' }, // This is just green.A700 as hex.
+  },
+});
+
+
+
 class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-      <div>
-        <Route path='/' component={Home} exact/>
-        <Route path='/contact' component={Contact}/>
-        <Route path='/portfolio' component={Portfolio}/>
-      </div>
-      </BrowserRouter>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+          <div>
+            <Route path='/' component={Home} exact />
+            <Route path='/contact' component={Contact} />
+            <Route path='/portfolio' component={Portfolio} />
+          </div>
+        </BrowserRouter>
+      </MuiThemeProvider>
     )
   }
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-import './Navigation.css'
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
-export default class Navigation extends React.Component {
-
-  render() {
-    return (
-      <span>NAV</span>
-    )
-  }
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
